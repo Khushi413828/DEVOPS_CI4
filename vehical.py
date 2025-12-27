8af6ea7026cfc2479b87de692b53618629f513eb
khushi <Khushi413828>
2025-12-27 12:30:27 +0530

    save

---
+++ vehical.py
@@ -0,0 +1,14 @@
+def vehical_details(vid,vname,price,yop):
+    result=(
+        f"vid={vid}"
+        f"vname={vname}"
+        f"price={price}"
+        f"yop={yop}"
+    )
+    return result
+if"__name__=__main":
+    vid=101
+    vname="car"
+    price=50000
+    ypo=2024
+    print(vehical_details("vid","vname","price","yop"))
